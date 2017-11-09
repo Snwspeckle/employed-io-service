c3e24a5fb0e9bf507874a6c35f022d3a59f75221
Tonald <twaldeck13@gmail.com>
2017-11-09 18:17:30 -0500

    Added functionality for returning one of the connections to a user bassed off of their email.  Gonig to return multiple connections later

--- src/main/kotlin/io/employed/service/AppConfig.kt
+++ src/main/kotlin/io/employed/service/AppConfig.kt
@@ -1,10 +1,12 @@
 package io.employed.service
 
+import com.fasterxml.jackson.databind.SerializationFeature
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter
 import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 
@@ -14,8 +16,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 class AppConfig : WebMvcConfigurerAdapter() {
     @Override
     override fun configureMessageConverters(converters: MutableList<HttpMessageConverter<*>>?) {
-        val protoBufHttpConverter = ProtobufHttpMessageConverter()
-        converters!!.add(protoBufHttpConverter)
+        val mappingJackson = MappingJackson2HttpMessageConverter()
+        mappingJackson.objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+
+        converters!!.add(ProtobufHttpMessageConverter())
+        converters.add(mappingJackson)
         super.configureMessageConverters(converters)
     }
 }
