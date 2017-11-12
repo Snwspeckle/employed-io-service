1eab89434d1f41334e76e07c4b4a43ac25d6a3e7
Aaron Triplett <aarondouglastriplett@gmail.com>
2017-11-12 12:38:36 -0800

    Add protobuffHttpMessageConverter as bean

--- src/main/kotlin/io/employed/service/Application.kt
+++ src/main/kotlin/io/employed/service/Application.kt
@@ -3,10 +3,17 @@ package io.employed.service
 import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter
 
 @SpringBootApplication
 class Application {
     private val log = LoggerFactory.getLogger(Application::class.java)
+
+    @Bean
+    fun protobufHttpMessageConverter(): ProtobufHttpMessageConverter {
+        return ProtobufHttpMessageConverter()
+    }
 }
 
 fun main(args: Array<String>) {
