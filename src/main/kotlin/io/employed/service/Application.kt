ae64678f10faf53845ee2822803392e6fe0d3fdd
Aaron Triplett <aarondouglastriplett@gmail.com>
2017-11-12 12:35:50 -0800

    Add correct dependencies and update Applicaiton class

--- src/main/kotlin/io/employed/service/Application.kt
+++ src/main/kotlin/io/employed/service/Application.kt
@@ -1,10 +1,13 @@
 package io.employed.service
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class Application
+class Application {
+    private val log = LoggerFactory.getLogger(Application::class.java)
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
