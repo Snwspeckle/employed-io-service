8b82cdf789217195f29e053c8b930ee9ed58da96
Aaron Triplett <aarondouglastriplett@gmail.com>
2018-03-17 20:05:37 -0700

    Update User APIs to include user creation and temporary generateMockUser

--- src/main/kotlin/io/employed/service/http/UserController.kt
+++ src/main/kotlin/io/employed/service/http/UserController.kt
@@ -1,14 +1,18 @@
 package io.employed.service.http
 
+import com.google.protobuf.util.Timestamps
 import io.employed.proto.User
 import io.employed.proto.Users
+import io.employed.service.persistence.toEntity
 import io.employed.service.persistence.toProto
 import io.employed.service.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 @RequestMapping(value = ["api"])
@@ -18,9 +22,25 @@ class UserController {
     lateinit var userRepository: UserRepository
 
     @RequestMapping(method = [(RequestMethod.GET)], value = ["/users"], produces = ["application/x-protobuf", "application/json"])
-    fun users() : Users = Users.newBuilder().addAllUser(userRepository.findAll().map { it.toProto() }).build()
+    fun getUsers(): Users = Users.newBuilder().addAllUser(userRepository.findAll().map { it.toProto() }).build()
 
     @RequestMapping(method = [(RequestMethod.GET)], value = ["/users/{id}"], produces = ["application/x-protobuf", "application/json"])
-    fun user(@PathVariable id: String) : User = userRepository.findByUserId(id).toProto()
+    fun getUserById(@PathVariable id: String): User = userRepository.findByUserId(id).toProto()
 
+    @RequestMapping(method = [(RequestMethod.POST)], value = ["/users"], produces = ["application/x-protobuf", "application/json"])
+    fun createUser(@RequestBody user: User): User = userRepository.insert(user.toEntity()).toProto()
+
+    @RequestMapping(method = [(RequestMethod.POST)], value = ["/users/mock"], produces = ["application/x-protobuf", "application/json"])
+    fun generateMockUserData(): User {
+        val newUser = User.newBuilder().setUserId(UUID.randomUUID().toString())
+            .setFirstName("Aaron")
+            .setLastName("Triplett")
+            .setBio("Hey, I'm Aaron.")
+            .setCreationDate(Timestamps.fromMillis(System.currentTimeMillis()))
+            .setEmail("AaronDouglasTriplett@gmail.com")
+            .setHandle("AaronTriplett")
+            .setPhoneNumber("567-342-2660")
+            .build()
+        return userRepository.insert(newUser.toEntity()).toProto()
+    }
 }
